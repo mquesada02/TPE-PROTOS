e151f91cb0fd3f8006f27bb157a920b853a16ae3
NCasella <nicolascasella02@gmail.com>
2024-07-08 10:09:53 -0300

    moved utils things to .h

--- client/include/utils.h
+++ client/include/utils.h
@@ -2,9 +2,9 @@
 #define TPE_PROTOS_UTILS_H
 
 #include <stdbool.h>
-
+#define MD5_SIZE 32
 
 int setupLeekerSocket(const char *service, const char **errmsg);
-
+bool calculateMD5(char* filename, char md5Buffer[MD5_SIZE + 1]);
 
 #endif //TPE_PROTOS_UTILS_H
--- client/lib/utils.c
+++ client/lib/utils.c
@@ -14,7 +14,7 @@
 
 #define SUCCESS 1
 #define ERROR -1
-#define MD5_SIZE 32
+
 #define MAX_FILENAME 256
 
 #define MAX_ADDR_BUFFER 128
@@ -129,7 +129,8 @@ int setupLeekerSocket   (const char *service, const char **errmsg) {
 bool calculateMD5(char* filename, char md5Buffer[MD5_SIZE + 1]) {
     FILE *file = NULL;
 
-    char buff[strlen("md5sum ") + MAX_FILENAME] = "";
+    char buff[strlen("md5sum ") + MAX_FILENAME];
+    memset(buff,0,strlen("md5sum ") + MAX_FILENAME);
     strcat(buff, "md5sum ");
     strcat(buff, filename);
 
